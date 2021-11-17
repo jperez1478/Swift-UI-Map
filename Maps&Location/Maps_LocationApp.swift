fb6b84a340c779923cd3b16090ddaca1599ca914
jperez1478 <jessicperez3@gmail.com>
2021-11-17 01:13:14 -0600

    Initial Commit

---
+++ Maps&Location/Maps_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maps_LocationApp.swift
+//  Maps&Location
+//
+//  Created by Jessica Perez on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Maps_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
